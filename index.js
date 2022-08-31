1a7f5e792463669708996dbf05508f50d4f8dd8f
Poostick949 <96563408+Poostick949@users.noreply.github.com>
2022-08-31 17:13:17 +0800

    Train Counter files. First attempt at HTML/JS

---
+++ index.js
@@ -0,0 +1,19 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+
+
+
+function increment(){
+    count += 1
+    countEl.textContent = count    
+}
+
+function save() {   
+    console.log(count)
+    saveEl.textContent += count + " - "
+    countEl.textContent = 0
+    count = 0
+};
+
+
